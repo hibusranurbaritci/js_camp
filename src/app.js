be5635d4a21991e53e6145f0c14b42e07a7b6627
hibusranurbaritci <busranurbaritci@gmail.com>
2021-10-20 11:19:49 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("merhaba kodlama.io")
+
+
+//dolarDun="Ankara"
+let  dolarBugun=9.30
+let dolarDun=9.20
+dolarDun="ankara"
+
+{
+   let  dolarDun =9.10
+}
+console.log(dolarDun)
+
+let konutKredileri =["konut kredisi","emlak konut kredisi","kamu konut kredisi","Ozel konut kredisi"]
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++){
+
+    console.log("<li>"+konutKredileri[i]+"</li>")
+
+
+}
+console.log("/<ul>")
+
